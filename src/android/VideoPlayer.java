279c58ea853294099937424bac642daf1fa70ff3
rhyanbridle <48830207+rhyanbridle@users.noreply.github.com>
2020-01-14 13:52:19 +0200

    Update VideoPlayer.java

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -143,6 +143,16 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
         // videoView.setVideoURI(uri);
         // videoView.setVideoPath(path);
         main.addView(videoView);
+        
+        //Try and hide the navidgation bar
+         View mDecorView = getWindow().getDecorView();
+         mDecorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
         player = new MediaPlayer();
         player.setOnPreparedListener(this);
