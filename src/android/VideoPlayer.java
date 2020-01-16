3d893685ddc9d49eed42a31719252fc210a84bbd
rhyanbridle <48830207+rhyanbridle@users.noreply.github.com>
2020-01-16 09:31:35 +0200

    Update VideoPlayer.java

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -131,7 +131,13 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
         dialog.setOnDismissListener(this);
         dialog.getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
 
-       
+       dialog.getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				//| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				//| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			);		
         
         // Main container layout
         LinearLayout main = new LinearLayout(cordova.getActivity());
