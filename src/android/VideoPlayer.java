751cafcfa2cf87009c12bf96d439325311408adf
xstremasoftware <brown.xstrema@gmail.com>
2020-03-27 18:24:25 +0000

    Update VideoPlayer.java

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -139,7 +139,7 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
 				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 			);		
         */
-	    View v = getWindow().getDecorView();
+	View v = dialog.getWindow().getDecorView();
         v.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
