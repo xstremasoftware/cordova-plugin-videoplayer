e913429d6686cec666f3939759b1939be3c2e1bd
xstremasoftware <brown.xstrema@gmail.com>
2020-03-31 15:46:46 +0100

    Update VideoPlayer.java

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -253,7 +253,7 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
         lp.height = WindowManager.LayoutParams.MATCH_PARENT;
 
         dialog.setContentView(main);
-	dialog.getActivity().getWindow().getDecorView().setSystemUiVisibility(ui_flags);	
+	getActivity().getWindow().getDecorView().setSystemUiVisibility(ui_flags);	
         dialog.show();
         dialog.getWindow().setAttributes(lp);
 	    
